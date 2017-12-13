0ad805e2cf222dca9459fdd935990d5d75903ff3
jackc852 <31644974+jackc852@users.noreply.github.com>
2017-12-12 20:46:36 -0800

    F-1005 added firebase and giphy integration

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+var config = {
+	apiKey: "AIzaSyC5YSojC81RtawxiAC0CaO9m9h1JuDwBtI",
+	authDomain: "talktoanonymous-d65df.firebaseapp.com",
+	databaseURL: "https://talktoanonymous-d65df.firebaseio.com",
+	projectId: "talktoanonymous-d65df",
+	storageBucket: "",
+	messagingSenderId: "709944500981"
+};
+firebase.initializeApp(config);
+
+
+
+function callGiphyAPI(character) {
+	var queryURL = `https://api.giphy.com/v1/gifs/search?api_key=FaVXq0SP86FKh8FYOgjpP6OzIcY5wMyg&limit=12&q=${character}`;
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function(response) {
+		//TODO - integrate with our requirements. Display function is not defined.
+		displayGifs(response);
+	}).fail(function(){
+		//TODO - integrate with our requirements. Display function is not defined.
+		displayErrors();
+	});
+}  
