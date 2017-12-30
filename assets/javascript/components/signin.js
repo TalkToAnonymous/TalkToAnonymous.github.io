7053337c8820f549495e3cc54d8ae974e07f29c6
jackc852 <31644974+jackc852@users.noreply.github.com>
2017-12-30 12:20:06 -0800

    add comments to js files

--- assets/javascript/components/signin.js
+++ assets/javascript/components/signin.js
@@ -1,3 +1,4 @@
+//Create sign in object, add functions to object.
 $(function () {
 	app.signIn = (function () {
 		var signInObj = function (firebaseUtil, goToSignUp) {
@@ -19,6 +20,9 @@ $(function () {
 			this.goToSignUp = goToSignUp;
 		};
 
+		//Initialize sign in page.
+		//Hide sign out button.
+		//Go to sign up page when sign up button is clicked.
 		signInObj.prototype.initialize = function () {
 			this.show();
 			$('#sign-out').addClass('is-hidden');
@@ -26,28 +30,34 @@ $(function () {
 			$('#go-to-sign-up').on('click', this.showSignUp);
 		};
 
+		//Show signup page when #go-to-sign-up is clicked.
 		signInObj.prototype.showSignUp = function(event) {
 			event.preventDefault();
 			this.goToSignUp();
 		}
 
+		//Show sign in container
 		signInObj.prototype.show = function (course) {
 			this.signInContainer.removeClass('is-hidden');
 		};
 
+		//Hide sign in container
 		signInObj.prototype.hide = function () {
 			this.signInContainer.addClass('is-hidden');
 		};
 
+		//Show error message for sign up errors
 		signInObj.prototype.showSignUpErrors = function(error) {
 			this.errorContainer.removeClass('is-hidden');
 			this.errorMessage.html(error.message);
 		};
 
+		//Hide sign up errors
 		signInObj.prototype.hideSignUpErrors = function() {
 			this.errorContainer.addClass('is-hidden');
 		};
 
+		//Send login info to firebase. If info is invalid show error message.
 		signInObj.prototype.signInUser = function(event) {
 			event.preventDefault();
 			this.hideSignUpErrors();
